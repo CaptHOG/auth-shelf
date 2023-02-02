8153380123b60628ec3b2981cf41edf1f091f0dd
Jeremy Hanson <rockbasser88@hotmail.com>
2023-02-02 14:22:02 -0600

    added table and form

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 function ShelfPage() {
   const dispatch = useDispatch();
+  const shelf = useSelector(store => store.shelf)
+  const [itemInput, setItemInput] = useState('');
+  const [urlInput, setUrlInput] = useState('');
 
   useEffect(() => {
     dispatch({
@@ -11,10 +14,69 @@ function ShelfPage() {
     })
   }, [])
 
+  const addItem = (event) => {
+    event.preventDefault();
+
+    let newItem = {
+      name: itemInput,
+      url: urlInput
+    }
+    console.log('newItem:', newItem)
+
+    dispatch({
+      type: 'ADD_ITEM',
+      payload: newItem
+    })
+  }
+
+  // test URL
+  // https://pngimg.com/uploads/apple/apple_PNG12405.png
   return (
     <div className="container">
       <h2>Shelf</h2>
-      <p>All of the available items can be seen here.</p>
+      <br/>
+      <h3>Add items to the shelf</h3>
+      <form>
+        <input
+          placeholder="Item Name"
+          type="text"
+          value={itemInput}
+          onChange={(event) => setItemInput(event.target.value)}
+        />
+        <input
+          placeholder="Image URL"
+          type="text"
+          value={urlInput}
+          onChange={(event) => setUrlInput(event.target.value)}
+        />
+      </form>
+      <button onClick={addItem}>Add Item</button>
+      <br/>
+      <table className="itemTable">
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Image</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {shelf.map((item) => {
+            return (
+              <tr key={item.id}>
+                <td>{item.description}</td>
+                <td>
+                  <img src={item.image_url} width="300px" height="200px"/>
+                </td>
+                <td>
+                  <button>Remove Item</button>
+                </td>
+              </tr>
+            )
+          })}
+          
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/redux/sagas/shelf.saga.js
+++ src/redux/sagas/shelf.saga.js
@@ -1,14 +1,11 @@
 import axios from "axios";
-// import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import { takeEvery, put } from "redux-saga/effects";
 
-function* getShelf() {
-  // const dispatch = useDispatch();
 
+function* getShelf() {
   try {
-    const response = yield axios.get('/shelf')
-    console.log(response.data);
+    const response = yield axios.get('/api/shelf')
+    // console.log(response.data);
     yield put({ type: 'SET_SHELF', payload: response.data })
   } catch (error) {
     console.error('Error getShelf in shelf.saga.js:', error);
