eefd6fa282b84ab4eae6e98ad3e00675f5cede02
Shong Lee <Shonglee17@gmail.com>
2023-02-02 11:38:50 -0600

    finished post/get route

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
-
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 /**
  * Get all of the items on the shelf
  */
@@ -12,16 +12,36 @@ router.get('/', (req, res) => {
 /**
  * Add an item for the logged in user to the shelf
  */
-router.post('/', (req, res) => {
+router.post('/',  (req, res) => {
+  let itemToSend = req.body;
   // endpoint functionality
+  const sqlQuery = `
+  INSERT INTO "item" ("description", "image_url", "user_id")
+    VALUES($1 , $2 , $3);`
+  const sqlValues = [itemToSend.description , itemToSend.image_url, itemToSend.user_id]
+
+  pool.query(sqlQuery, sqlValues)
+      .then((dbRes) => {
+          res.sendStatus(201);
+      })
+      .catch((dbErr) => {
+          console.log(`error in POST: serverside`, dbErr);
+          res.sendStatus(500);
+      });
+
 });
 
 /**
  * Delete an item if it's something the logged in user added
  */
-router.delete('/:id', (req, res) => {
-  // endpoint functionality
+router.delete('/:id', rejectUnauthenticated, (req, res) => {
+  let itemId = req.params.id
+  let userId = req.body.user_id
+  // const sqlQuery = `DELETE FROM "item"
+	//   WHERE  "user_id" = $1`;
 });
+
+const sqlValues = [userId]
 
 /**
  * Update an item if it's something the logged in user added
